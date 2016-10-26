0d0b5fed508d30c5102464f5e0f861dfb05c5837
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-10-26 14:57:32 +0100

    all basic thermostat features added

--- jasmine-standalone-2.5.2/spec/ThermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 describe ("testing temperature", function (){
-    var thermostat;
+  var thermostat;
 
-    beforeEach(function() {
-      thermostat = new Thermostat();
-    });
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   it ("default temperature is set to 20degrees", function() {
     expect(thermostat.temperature()).toBe(20);
@@ -13,20 +13,63 @@ describe ("testing temperature", function (){
 
   it ("calling up increase temperature", function () {
     thermostat.temperatureUp();
-    expect(thermostat.temperature()).toBe(25);
+    expect(thermostat.temperature()).toBe(21);
   });
 
   it ("calling up decrease temperature", function () {
     thermostat.temperatureDown();
-    expect(thermostat.temperature()).toBe(15);
+    expect(thermostat.temperature()).toBe(19);
   });
 
   it ("can't go below 10degrees", function(){
-    for (var i = 1; i < 4; i++) {
+    for (var i = 1; i < 11; i++) {
       thermostat.temperatureDown();
-      };
-    expect(thermostat.temperature()).toBe(10)
-
+    };
+    expect(thermostat.temperature()).toBe(10);
   });
-  
+
+  it ("can't go above 25 degrees", function(){
+    for (var i = 1; i < 6; i++) {
+      thermostat.temperatureUp();
+    };
+    expect(thermostat.temperature()).toBe(25);
+  });
+
+  it ("resets to 20degress", function(){
+    thermostat.temperatureUp();
+    thermostat.reset();
+    expect(thermostat.temperature()).toBe(20);
+  });
+
+  describe ("power saving off", function() {
+
+    it ("can't go above 32 degrees", function(){
+      thermostat.powerSaving();
+      for(var i =1; i< 14; i++) {
+        thermostat.temperatureUp();
+      };
+      expect(thermostat.temperature()).toBe(32);
+
+    });
+  });
+
+  describe ("checking usage", function() {
+    it ("expects to be Medium, temp = <25", function() {
+      expect(thermostat.usage()).toBe("Medium-usage");
+    });
+    it ("expects to be Low, temp = <18", function() {
+      for(var i =1; i< 5; i++) {
+        thermostat.temperatureDown();
+      };
+      expect(thermostat.usage()).toBe("Low-usage");
+    });
+    it ("expects to be high, temp = => 25", function() {
+      thermostat.powerSaving();
+      for(var i =1; i< 6; i++) {
+        thermostat.temperatureUp();
+      };
+      expect(thermostat.usage()).toBe("High-usage");
+    });
+  });
+
 });
--- jasmine-standalone-2.5.2/src/Thermostat.js
+++ jasmine-standalone-2.5.2/src/Thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat() {
   this._temperature = 20;
+  this._powersaving = true;
 };
 
 Thermostat.prototype.temperature = function() {
@@ -9,10 +10,32 @@ Thermostat.prototype.temperature = function() {
 };
 
 Thermostat.prototype.temperatureUp = function() {
-  this._temperature += 5;
+  if (this._powerSaving) {
+    this._temperature =  this._temperature  < 25 ? this._temperature + 1 : 25;
+  } else {
+    this._temperature =  this._temperature  < 32 ? this._temperature + 1 : 32;
+  }
 };
 
 Thermostat.prototype.temperatureDown = function() {
-  this._temperature =  this._temperature  > 10 ? this._temperature - 5 : 10;
+  this._temperature =  this._temperature  > 10 ? this._temperature - 1 : 10;
 
+};
+
+Thermostat.prototype.powerSaving = function (){
+  this._powersaving = !this._powersaving;
+};
+
+Thermostat.prototype.reset = function () {
+  this._temperature = 20
+};
+
+Thermostat.prototype.usage = function () {
+  if (this._temperature < 18) {
+    return "Low-usage";
+  }
+  else if (this._temperature < 25 ) {
+    return "Medium-usage";
+  }
+    return "High-usage";
 };
